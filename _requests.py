7b5c3155f7ed9ffb26724520258ef86089c91ab8
SahilMehdiyev <branchsahil@gmail.com>
2024-06-20 15:54:17 +0400

    Request method add

--- _requests.py
+++ _requests.py
@@ -1,18 +1,65 @@
+# import requests
+# import json
+
+# api_key = '33d962541739574c973b9e1b'
+# api_url = f'https://v6.exchangerate-api.com/v6/{api_key}/latest/'
+
+# bozulan_doviz = input('Bozulan doviz turu: ') 
+# alinan_doviz = input('Alinan doviz turu: ') 
+# miktar = input(f'Ne kadar {bozulan_doviz} bozdurmak istiyorsunuz: ')
+
+# sonuc = requests.get(api_url + bozulan_doviz)
+
+# sonuc_json = json.loads(sonuc.text)
+
+# # print(sonuc_json['conversion_rates'][alinan_doviz])
+
+# print('1 {0} = {1} {2}'.format(bozulan_doviz,sonuc_json['conversion_rates'][alinan_doviz],alinan_doviz))
+
+
+# Web Scraper
+
+# import requests
+# from bs4 import BeautifulSoup
+
+# # Veri toplanacak web sayfasının URL'si
+# url = 'https://www.example.com'
+
+# response = requests.get(url)
+
+# soup = BeautifulSoup(response.text, 'html.parser')
+
+# basliklar = soup.find_all('h1')
+
+# for baslik in basliklar:
+#     print(baslik.text)
+
 import requests
-import json
+from bs4 import BeautifulSoup
 
-api_key = '33d962541739574c973b9e1b'
-api_url = f'https://v6.exchangerate-api.com/v6/{api_key}/latest/'
+# Web sayfasına istek gönderin
+url = "http://sahilmehdiyev.com.tr"  # İstediğiniz web sayfasının URL'sini buraya yazın
+response = requests.get(url)
 
-bozulan_doviz = input('Bozulan doviz turu: ') 
-alinan_doviz = input('Alinan doviz turu: ') 
-miktar = input(f'Ne kadar {bozulan_doviz} bozdurmak istiyorsunuz: ')
+# İstek başarılı oldu mu kontrol edin
+if response.status_code == 200:
+    # HTML içeriğini ayrıştırın
+    soup = BeautifulSoup(response.content, 'lxml')
+    
+    # Sayfadaki tüm metinleri alın
+    page_text = soup.get_text()
+    
+    # Hedef kelimeyi arayın
+    target_word = "sahil"  # Aradığınız kelimeyi buraya yazın
+    if target_word in page_text:
+        print(f"'{target_word}' kelimesi sayfada bulundu.")
+    else:
+        print(f"'{target_word}' kelimesi sayfada bulunamadı.")
+else:
+    print(f"İstek başarısız oldu, durum kodu: {response.status_code}")
 
-sonuc = requests.get(api_url + bozulan_doviz)
+# Ekstra: Hedef kelimeyi içeren metinleri bulma
+texts_containing_word = [sentence for sentence in page_text.split() if target_word in sentence]
+print(f"'{target_word}' kelimesini içeren metinler: {texts_containing_word}")
 
-sonuc_json = json.loads(sonuc.text)
-
-# print(sonuc_json['conversion_rates'][alinan_doviz])
-
-print('1 {0} = {1} {2}'.format(bozulan_doviz,sonuc_json['conversion_rates'][alinan_doviz],alinan_doviz))
 
---
+++ test.py
@@ -0,0 +1,29 @@
+# def remainder(x,y):
+#     return x % y 
+
+# print(remainder(3,4))
+
+
+# def string_int(txt):
+#     try:
+#         return int(txt)
+#     except ValueError:
+#         print(f"Error: '{txt}' is not a valid integer.")
+#         return None
+
+# print(string_int("111"))
+
+
+# Create a function that takes a list containing only numbers and return the first element.
+
+
+# def get_first_value(number_list):
+#     return number_list[0]
+
+# print(get_first_value([1, 2, 3]))      # Output: 1
+
+
+# def addition(num):
+#     return num +1 
+
+# print(addition(5))  
